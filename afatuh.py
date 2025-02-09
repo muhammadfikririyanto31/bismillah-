bc908a8813b6144ea3b6114a76c6ad9f311a1452
muhammadfikririyanto31 <shastutik32@gmail.com>
2025-02-09 10:39:47 +0700

    Update afatuh.py

--- afatuh.py
+++ afatuh.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import numpy as np
-from skimage.morphology import skeletonize
 from skimage.feature import hog
 from skimage import color, exposure
 from skimage.transform import resize
