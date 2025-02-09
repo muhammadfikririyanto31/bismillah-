aff3bde9275f29192d9763373a67442e0ed696af
muhammadfikririyanto31 <shastutik32@gmail.com>
2025-02-09 11:41:53 +0700

    Update afatuh.py

--- afatuh.py
+++ afatuh.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
-import cv2
 import tensorflow as tf
 from skimage.morphology import skeletonize
 from skimage.feature import hog
